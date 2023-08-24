01d97f4d7926ce6d3ed191c980799d5be3c1cb64
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-24 14:27:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import TheNavbar from "./components/TheNavBar.vue";
+
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
+<template>
+  <TheNavbar></TheNavbar>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,16 @@
+<script>
+/* import  from './components/'; */
+
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
