06ee7259f426446820d8fc6dc1bea559f7b04e3f
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-28 15:09:03 +0200

    fix import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import TheMain from "./components/TheMain.vue";
-import TheNavbar from "./components/TheNavBar.vue";
+import TheNavbar from "./components/TheNavbar.vue";
 
 export default {
   components: {
